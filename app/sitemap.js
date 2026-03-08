df36f3f98c236d60bd3998680d7c2616a52e28fb
aggrk <kennedy.phinias@gmail.com>
2026-03-08 12:08:06 +0300

    -

---
+++ app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://sayukhaconstruction.co.tz",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    // {
+    //   url: "https://sayukhaconstruction.co.tz/about",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: "https://sayukhaconstruction.co.tz/services",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: "https://sayukhaconstruction.co.tz/contact",
+    //   lastModified: new Date(),
+    //   changeFrequency: "monthly",
+    //   priority: 0.7,
+    // },
+  ];
+}
